c572981f6376c1b7ed34b950ae6093fc4ca3a121
cmbarros <carlosmbarros@icloud.com>
2024-05-15 07:04:39 -0700

    Initial Commit

---
+++ Trivia Tecnologica/Trivia_TecnologicaApp.swift
@@ -0,0 +1,17 @@
+//
+//  Trivia_TecnologicaApp.swift
+//  Trivia Tecnologica
+//
+//  Created by Carlos Barros on 5/15/24.
+//
+
+import SwiftUI
+
+@main
+struct Trivia_TecnologicaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
